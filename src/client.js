3da8ed0653ff132d6493b7119e4b7d43442be625
Joe Pea <joe@trusktr.io>
2018-11-06 15:23:59 -0800

    show how worker loaded from Object URL is unable to require dependencies

--- src/client.js
+++ src/client.js
@@ -2,4 +2,13 @@ const path = require('path')
 
 console.log('make a worker: ', path.resolve(__dirname, 'worker.js'))
 
-const worker = new Worker(path.resolve(__dirname, 'worker.js'))
+// this works:
+// const worker = new Worker(path.resolve(__dirname, 'worker.js'))
+
+// this doesn't work:
+const fs = require('fs')
+const source = fs.readFileSync(path.resolve(__dirname, 'worker.js'), 'utf8')
+const sourceUrl = URL.createObjectURL(
+	new Blob([source], { type: 'text/javascript' }),
+)
+const worker = new Worker(sourceUrl)
